745178f47601548a698896538851f930a0b3f111
Juffins <mimeev.andrey@yandex.ru>
2022-07-23 17:19:02 +0500

    Масштабирована задача о счасливом номере.

--- src/LuckyNumber.java
+++ src/LuckyNumber.java
@@ -2,36 +2,63 @@ import java.util.Scanner;
 
 public class LuckyNumber {
 
+    public static Scanner scanner = new Scanner(System.in);
+
     public static void CheckNumber() {
-        System.out.print("Enter six-digit number: ");
+        System.out.print("Enter an even positive number: ");
 
-        Scanner scanner = new Scanner(System.in);
-        int number = scanner.nextInt();
-
-        int requiredLength = 6;
-        int minSixDigitNumber = 100000;
-        int maxSixDigitNumber = 999999;
-
-        if (number < minSixDigitNumber || number > maxSixDigitNumber){
-            System.out.println("Please, enter a positive six-digit number!");
+        String sNumber;
+        int number;
+        int length;
+        sNumber = InputNumber();
+        length = sNumber.length();
+        try {
+            number = Integer.valueOf(sNumber);
+        } catch (Exception e) {
+            e.printStackTrace();
             return;
         }
 
-        int[] digits = new int[requiredLength];
+        int sumFirstHalf = 0;
+        int sumSecondHalf = 0;
+        int firstDigit;
+        int secondDigit;
+        int numberOfDigits = length;
 
-        for(int i = 5; i >= 0; i--){
-            digits[i] = number % 10;
+        for(int i = 0; i < length; i++, numberOfDigits -= 2){
+            secondDigit = number % 10;
+            sumSecondHalf += number % 10;
+            firstDigit = (number / Math.max((int)Math.pow(10, numberOfDigits - 1), 1));
+            sumFirstHalf += firstDigit;
+            number -= firstDigit * (int)Math.pow(10, numberOfDigits - 1);
+            number -= secondDigit;
             number /= 10;
         }
 
-        int SumFirstThree = digits[0] + digits[1] + digits[2];
-        int SumSecondThree = digits[3] + digits[4] + digits[5];
-
-        if(SumFirstThree == SumSecondThree) {
+        if(sumFirstHalf == sumSecondHalf) {
             System.out.println("Congratulations! It's a lucky number!!!");
         }
         else {
             System.out.println("Better luck next time!");
         }
+    }
+
+    public static String InputNumber(){
+        int number;
+        String sNumber;
+        if (scanner.hasNextInt()) {
+            number = scanner.nextInt();
+            sNumber = Integer.toString(number);
+            if(sNumber.length() % 2 != 0 || number < 0){
+                System.out.println("Incorrect number! Please, enter an even positive number.");
+                sNumber = InputNumber();
+            }
+        }
+        else {
+            System.out.println("Incorrect number! Please, enter an even positive number.");
+            scanner.next();
+            sNumber = InputNumber();
+        }
+        return sNumber;
     }
 }
